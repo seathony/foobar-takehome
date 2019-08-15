6e6bc5f156de0de6b43f84817712d2165f6d3c98
seathony <seathony@gmail.com>
2019-08-15 19:31:06 -0400

    add functions to call per instructions

--- lib/foobar_web/controllers/page_controller.ex
+++ lib/foobar_web/controllers/page_controller.ex
@@ -5,14 +5,19 @@ defmodule FoobarWeb.PageController do
     render(conn, "index.html")
   end
 
+#  call functions per instructions
+  def fooBarFunction() do "foobar" end
+  def fooFunction() do "foo" end
+  def barFunction() do "bar" end
+
   def fizzbuzz(conn, _params) do
 
 #    determine between foo or bar
     isFoobar = fn (arg) ->
       cond do
-        arg["foo"] == "true" && arg["bar"] == "true" -> "foobar"
-        arg["foo"] == "true" -> "foo"
-        arg["bar"] == "true" -> "bar"
+        arg["foo"] == "true" && arg["bar"] == "true" -> fooBarFunction()
+        arg["foo"] == "true" -> fooFunction()
+        arg["bar"] == "true" -> barFunction()
         true -> "not foo or bar"
        end
     end
